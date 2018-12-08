ba07920d9b07163c74763f123290cdce15ae7a29
Rafael Blanco <rafaelblanco@Rafaels-MacBook-Pro-2.local>
2018-12-08 13:02:09 -0500

    Update back end files

---
+++ controllers/chatController.js
@@ -0,0 +1,21 @@
+var express = require("express");
+
+var router = express.Router();
+
+var chat = require("../model/chat.js");
+
+router.get("/", function(req, res){
+    
+});
+
+router.post("/api/chat", function(req, res){
+
+});
+
+router.put("/api/chat", function(req, res){
+
+});
+
+router.delete("/api/chat", function(){
+
+});
