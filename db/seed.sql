8d955298414b6edd5840725e1576678348ff85f2
Alejandro Loja <alejandro.loja@gmail.com>
2018-12-15 14:38:27 -0500

    Few updates

--- db/seed.sql
+++ db/seed.sql
@@ -2,10 +2,15 @@ INSERT INTO users(user_name) VALUES ('Meili');
 INSERT INTO users(user_name) VALUES ('Rafael');
 INSERT INTO users(user_name) VALUES ('Alejandro');
 
-INSERT INTO messages(message, user) VALUES("Hello world.", "Meili");
-INSERT INTO messages(message, user) VALUES("I like to work on the back end.", "Rafael");
-INSERT INTO messages(message, user) VALUES("I like to work on the front end.", "Alejandro");
-INSERT INTO messages(message, user) VALUES("I like cats.", "Meili");
+INSERT INTO chats(message, user) VALUES("Hello world.", "Meili");
+INSERT INTO chats(message, user) VALUES("I like to work on the back end.", "Rafael");
+INSERT INTO chats(message, user) VALUES("I like to work on the front end.", "Alejandro");
+INSERT INTO chats(message, user) VALUES("I like cats.", "Meili");
+
+INSERT INTO logins(userlogin) VALUES("Alejandro");
+
+
 
 SELECT * FROM users;
-SELECT * FROM messages; 
+SELECT * FROM messages;
+SELECT * FROM logins;
--- public/assets/js/chats.js
+++ public/assets/js/chats.js
@@ -1,56 +1,154 @@
 console.log('chats.js connected.')
 
 $(document).ready(function () {
+  // Run Modal 
+  $.ajax("/allusers", {
+    type: "GET"
+  }). then(function(response){    
+    for (var i=0; i < response.users.length; i++) {
+    // console.log(i)
+    var eachUser = response.users[i].user_name;
+    console.log(eachUser)
+    var newDiv = $('<button type="button" class="col btn btn-primary user"></button>');
+    // <button type="button" data-id="Rafael" class="col btn btn-primary user">Rafael</button>
+    newDiv.attr('data-id',eachUser)
+    newDiv.text(eachUser)
+    // console.log(newDiv)
+    $('#all-users').append(newDiv)
+    }
 
-
-    $('#submit').on('click', function () {
-        event.preventDefault();
-        console.log('Hello')
-
-      var userMessage = $("#m").val();
-      console.log(userMessage)
-      var newUserMessage = {
-          user: "name",
-          message: userMessage
-      }
-      
-        $.ajax("/api/chat",{
-        type: "POST",
-        data: newUserMessage
-      })
-        .then(function (response) {
-          console.log(response)
-          console.log("Sent message: ", newUserMessage);           
-          location.reload(); 
-        })
-    });
-
-    $("#delete").on("click", function(event) {
-      var id = $(this).data("id");
-  
-      // Send the DELETE request.
-      $.ajax("/api/chat/" + id, {
-        type: "DELETE"
-      }).then(
-        function() {
-          console.log("deleted chat", id);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-        );
+    changeUser();
   });
-});
- 
- 
- 
+
+  var runModal = 'run';
+  console.log('initial: ' + runModal);
+
+  // $(window).on('load', function () {
+  //   console.log('Connected to Webpage.')
+  //   if (runModal === 'run') {
+  //     $('#myModal').modal('show');
+  //     runModal = "don't run";
+  //     console.log('current: ' + runModal)
+  //   }
+  // });
+
+  $('#messages').animate({ scrollTop: document.body.scrollHeight }, "fast");
+  var changeUser = function () {
+  $('.user-chosen').on('click', function () {
+    var chosenUser = $(this).data('id');
+    console.log(chosenUser)
+    $("log-out").data(chosenUser);
+
+    $.ajax("/api/chat", {
+      type: "POST"
+    }). then(function(response){
+      console.log(response);
+      location.reload()
+    });
+  });
+}
+
+  $('.user').on('click', function () {
+    var chosenUser = $(this).data('id');
+    console.log(chosenUser)
+
+    $('#logged-in').html(chosenUser);
+
+    // $.ajax("/api/chat", {
+    //   type: "POST"
+    // }). then(function(response){
+    //   console.log(response);
+    //   location.reload()
+    // });
+  });
+
+  // $('#submit').keypress(function (e) {
+
+  //   var code = e.keyCode || e.which;
+  //   if (code == 13) { //Enter keycode
+  //     console.log("Pressed ENTER")
+  //   }
+  // });
+  $('.edit').click(function(){
+    $(this).hide();
+    $('.messages').addClass('editable');
+    $('.text').attr('contenteditable', 'true');  
+    $('.save').show();
+  });
+  
+  $('.save').click(function(){
+    $(this).hide();
+    $('.messages').removeClass('editable');
+    $('.text').removeAttr('contenteditable');
+    $('.edit').show();
+  });
+
+
+  $('#submit').on('click', function () {
+    event.preventDefault();
+    var id = $(this).data("id");
+
+    console.log('Hello')
+    var userName = $("#user").val()
+    var userMessage = $("#m").val();
+    console.log(userMessage)
+    var newUserMessage = {
+      user: userName,
+      message: userMessage
+    }
+    console.log(newUserMessage);
+
+    $.ajax("/api/chat", {
+      type: "POST",
+      data: newUserMessage
+    })
+      .then(function (response) {
+        console.log(response)
+        console.log("Sent message: ", newUserMessage);
+        location.reload();
+        $("#messages").scrollTop = $("#messages").scrollHeight - $("#messages").clientHeight;
+      })
+  });
+
+  $(".delete").on("click", function (event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/chat/" + id, {
+      type: "DELETE"
+    }).then(
+      function () {
+        console.log("deleted chat", id);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+
+
+  $(".save").on("click", function (event) {
+    var id = $(this).parent().data("id");
+    console.log(id)
+    
+    var messages = $(this).text()
+
+    $.ajax("api/chat/" + id, {
+      type: "PUT",
+      data: messages
+    }).then(
+      function () {
+        console.log("update chat", id);
+        location.reload();
+      }
+    );
+  });
+
   // function getMessages() {
   //   $('#submit').on('click', function () {
   //     console.log('Hello');
 
-  //     var userMessage = $(this).data("userMessage");
-  //     var newSleep = $(this).data("newsleep");
 
-  //     var queryURL = '/'
 
   //     $.ajax("/api/chat",{
   //       url: queryURL,
@@ -62,14 +160,20 @@ $(document).ready(function () {
   //   });
   // };
 
+  function poll() {
+    setTimeout(function () {
+      $.ajax({
+        url: "http://localhost:8080/", type: "GET", success: function (data) {
+          //Setup the next poll recursively
+          poll();
+
+          console.log("IM POLLING")
+
+        }, dataType: "json"
+      });
+    }, 5000);
+  };
+
+  poll();
+});
   // getMessages();
-
-
-
-
-
-
-
-
-
-
